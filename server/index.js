65595ae5a01696ed538c56a8b40f536c0cacc68d
vishh2dev <vishh2dev@gmail.com>
2023-01-15 20:36:03 +0530

    connect to database

--- server/index.js
+++ server/index.js
@@ -1,4 +1,23 @@
 import express from 'express';
-import bodyParser from 'body-parse';
+import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+
+const app = express();
+
+app.use(bodyParser.json({limit: "30mb", extended: true}))
+app.use(bodyParser.urlencoded({limit: "30mb", extended: true}))
+app.use(cors())
+
+const CONNECTION_URL = 'mongodb+srv://vishnu:OcEd2eUT2xKSaQk7@cluster0.0ce6wj3.mongodb.net/?retryWrites=true&w=majority'
+const PORT = process.env.port || 5000;
+
+mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true })
+        .then(()=>{
+            app.listen(PORT,()=> console.log(`server running on port : ${PORT}`))
+        })
+        .catch((error) =>{
+            console.log(error.message)
+        })
+
+// mongoose.set('useFindAndModify', false)
