23bf53c11b8b65cc6a65fe74baaa0497682859ad
vishh2dev <vishh2dev@gmail.com>
2023-01-25 23:45:42 +0530

    passing the id for updation

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -5,7 +5,7 @@ import {Grid, CircularProgress } from '@material-ui/core'
 import Post from "./Post/Post";
 
 import useStyles from './styles'
-const Posts = () =>{
+const Posts = ({setCurrentId}) =>{
     const classes = useStyles()
 
     const posts = useSelector((state) => state.posts)
@@ -16,7 +16,7 @@ const Posts = () =>{
                 {
                     posts.map((post) =>(
                         <Grid  key={post.id} item xs={12} sm={6}>
-                            <Post post={post}/>
+                            <Post post={post} setCurrentId={setCurrentId}/>
                         </Grid>
                     ))
                 }
