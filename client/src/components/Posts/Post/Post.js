09ac34824739f5c8db21c8c6a471b00a15e15589
vishh2dev <vishh2dev@gmail.com>
2023-01-27 23:05:50 +0530

    title added to post

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -8,6 +8,7 @@ import useStyles from './styles'
 
 const Post = ({post,setCurrentId}) =>{
     const classes = useStyles()
+  
     return(
         <Card className={classes.card } >
             <CardMedia className={classes.media} image={post.selectedFile } title={post.title } />
@@ -16,15 +17,16 @@ const Post = ({post,setCurrentId}) =>{
                 <Typography variant="body2" >{moment(post.createdAt).fromNow()}</Typography>
             </div>
             <div className={classes.overlay2} >
-                <Button style={{color: 'white'}} size="small" onClick={() => {setCurrentId(post._id)}} >
+                <Button style={{color: 'white'}} size="small" onClick={() => setCurrentId(post._id)} >
                     <MoreHorizIcon fontSize="default" />
                 </Button>
             </div>
             <div className={classes.details} >
                 <Typography variant="body2" color="textSecondary" >{post.tags.map((tag) => `#${tag}` )}</Typography>
             </div>
+            <Typography className={classes.title} variant="h5"  gutterBottom>{post.title}</Typography>
             <CardContent>
-                <Typography className={classes.title} variant="h5"  gutterBottom>{post.message}</Typography>
+                <Typography variant="h5"  gutterBottom>{post.message}</Typography>
             </CardContent>
             <CardActions className={classes.cardActions} >
                 <Button size="small" color="primary" onClick={()=>{}} >
