901d8edbfdb5d640d015378d90b6c14a6dea3e3a
vishh2dev <vishh2dev@gmail.com>
2023-02-06 22:33:46 +0530

    env added

--- server/index.js
+++ server/index.js
@@ -2,21 +2,21 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
-
+import dotenv from 'dotenv'
 import postRoutes from './routes/posts.js'
 
 const app = express();
-
+dotenv.config()
 
 app.use(bodyParser.json({limit: "30mb", extended: true}))
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}))
 app.use(cors())
 
 app.use('/posts', postRoutes)
-const CONNECTION_URL = 'mongodb+srv://vishnu:OcEd2eUT2xKSaQk7@cluster0.0ce6wj3.mongodb.net/?retryWrites=true&w=majority'
-const PORT = process.env.port || 5000;
 
-mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true })
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(process.env.CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true })
         .then(()=>{
             app.listen(PORT,()=> console.log(`server running on port : ${PORT}`))
         })
