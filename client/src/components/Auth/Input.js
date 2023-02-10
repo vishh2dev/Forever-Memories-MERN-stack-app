e97fecc12d3c4166a66fcc577aae7b599a471944
vishh2dev <vishh2dev@gmail.com>
2023-02-10 20:30:45 +0530

    input component for signin and signup

---
+++ client/src/components/Auth/Input.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import { TextField,Grid,InputAdornment,IconButton } from '@material-ui/core'
+
+import Visibilty from '@material-ui/icons/Visibility'
+import VisibiltyOff from '@material-ui/icons/VisibilityOff'
+
+const Input = ({half,name,handleChange,label,autoFocus,type,handleShowPassword}) => {
+    
+  return (
+    <Grid item xs={12} sm={half ? 6 : 12} >
+        <TextField 
+            name={name}
+            onChange={handleChange}
+            variant="outlined"
+            required
+            fullWidth
+            label={label}
+            autoFocus={autoFocus}
+            type={type}
+            InputProps={name === 'password' && {
+                endAdornment: (
+                    <InputAdornment position="end" >
+                        <IconButton onClick={handleShowPassword} >
+                            {type === 'password' ? <Visibilty/> : <VisibiltyOff/>}
+                        </IconButton>
+                    </InputAdornment>
+                )
+            }} 
+        />
+    </Grid>
+  )
+}
+
+export default Input
